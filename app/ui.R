64892bf452e80fc934327d8e0e97a106bc91b4f2
jillymackay <jill.mackay@ed.ac.uk>
2023-05-18 13:56:36 +0100

    Add first text to rmd and ui

--- app/ui.R
+++ app/ui.R
@@ -45,6 +45,27 @@ shinyUI(
                       fluidPage(fluidRow(tags$p("You can download a word document version of this page by clicking below."),
                                          downloadButton(outputId = "report",
                                                         label = "Generate Downloadable Report")),
+                                fluidRow(tags$h1("2022 Graduate and VetGDP Adviser Surveys"),
+                                         tags$h2("About this report"),
+                                         tags$p("This report was generated based on the Royal College of Veterinary Surgeons (RCVS) Education Outcomes Data from 
+                                                Graduates and Advisers received in 2023. It was generated from the full redacted data provided by the RCVS for the 
+                                                purposes of additional analyses for quality improvement and processed via the R(D)SVS app.", 
+                                                tags$a(href="https://github.com/jillymackay/VSC_2023_VetGDP", "More information about the app can be found here")),
+                                         tags$p("This data was collected by the RCVS, and the R(D)SVS app has no input into question formation or data collection."),
+                                         tags$p("Graduates respond to the RCVS survey prior to setting up their personal e-portfolio. Advisers were matched with 
+                                                graduates to act as a mentor and asked to complete the survey  approximately 6 weeks after the graduate had started their 
+                                                programme to allow time for skills to be assessed. Advisers worked only with 2022 graduates."),
+                                         tags$p("The majority of questions within the survey are presented in the following format:"),
+                                         tags$em("How well prepared was the graduate to [conduct skill] at the point of graduation?",
+                                                tags$li("1. (Not at all prepared)"),
+                                                tags$li("2."),
+                                                tags$li("3."),
+                                                tags$li("4."),
+                                                tags$li("5. (Completely prepared)")),
+                                         tags$br(),
+                                         tags$p("There are 105 clinical skill related questions and 14 professional skill related questions. Graduates were also asked to rate 
+                                                their overall confidence to work independently when starting their first role on a scale from 1 (Not at all confident) to 5 
+                                                (Extremely confident).")),
                                 fluidRow(column(width = 6,
                                                 plotOutput(outputId = "testplotgrad")),
                                          column(width = 6, 
